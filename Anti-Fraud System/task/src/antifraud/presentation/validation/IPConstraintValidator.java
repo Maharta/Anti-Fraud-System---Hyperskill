3fe2f1393c184064626622766e733f276d3a17f5
Maharta <gedemaharta.dev@gmail.com>
2023-04-23 20:54:51 +0800

    Updated Annotation and ConstraintValidator for PathVariable validation.

--- Anti-Fraud System/task/src/antifraud/presentation/validation/IPConstraintValidator.java
+++ Anti-Fraud System/task/src/antifraud/presentation/validation/IPConstraintValidator.java
@@ -6,6 +6,11 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class IPConstraintValidator implements ConstraintValidator<ValidIP, String> {
+
+    @Override
+    public void initialize(ValidIP constraintAnnotation) {
+    }
+
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null) {
--- Anti-Fraud System/task/src/antifraud/presentation/validation/ValidIP.java
+++ Anti-Fraud System/task/src/antifraud/presentation/validation/ValidIP.java
@@ -5,7 +5,7 @@ import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Target({ElementType.FIELD})
+@Target({ElementType.PARAMETER, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = IPConstraintValidator.class)
 public @interface ValidIP {
